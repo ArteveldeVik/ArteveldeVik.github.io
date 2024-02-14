e56c01fc48b0533901d7d8954e9b49386c1b6460
ArteveldeVik <viksluij@student.arteveldehs.be>
2024-02-14 11:59:37 +0100

    Aanpassingen aan index + toeveoegen van scripts

---
+++ index.js
@@ -0,0 +1,27 @@
+const body = document.querySelector('body')
+
+const getJson = async () => {
+    const url = await fetch("index.json")
+    const data = await url.json()
+    return data
+}
+
+getJson()
+.then((data) => {
+    const h1 = document.createElement("h1")
+    const p = document.createElement("p")
+    const ul = document.createElement("ul")
+    const img = document.createElement("img")
+
+    h1.innerHTML = data.titel
+    p.innerHTML = data.omschrijving
+    img.innerHTML = data.afbeelding
+
+    body.appendChild(h1, p, ul, img)
+    
+    data.technology.forEach(elm => {
+        const li = document.createElement('li')
+        li.innerHTML = elm
+        li.appendChild(ul)       
+    });
+})
